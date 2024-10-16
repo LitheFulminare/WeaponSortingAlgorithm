316159db35f6c2958f4d990c80726d36739cf819
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-16 12:12:42 -0300

    fixed copy file error

--- WeaponSorting/Program.cs
+++ WeaponSorting/Program.cs
@@ -1,7 +1,14 @@
 ﻿using WeaponSorting;
 
-string weaponsPath = "weapons.txt";
+string weaponsPath = "data/weapons.txt";
 
 List<Weapon> weapons = new List<Weapon>();
 
 weapons.AddRange(FileReader.CreateWeaponList(weaponsPath));
+
+Console.WriteLine(weapons.Count);
+
+foreach (Weapon weapon in weapons)
+{
+    Console.WriteLine(weapon);
+}
