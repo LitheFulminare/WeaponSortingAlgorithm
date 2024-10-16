b73d333190ab7475d4fe3662dcf676b14d5d8cc9
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-16 12:31:42 -0300

    setup ler user input

--- WeaponSorting/Program.cs
+++ WeaponSorting/Program.cs
@@ -6,7 +6,18 @@ List<Weapon> weapons = new List<Weapon>();
 
 weapons.AddRange(FileReader.CreateWeaponList(weaponsPath));
 
-foreach (Weapon weapon in weapons)
+//PrintAllWeapons();
+
+// criar um while que repete essas frases até o user colocar um input valido
+Console.WriteLine("Digite qual algorítimo deve ser usado para ordenar as armas");
+Console.WriteLine("1 - Merge Sort");
+Console.WriteLine("2 - Quick Sort");
+string userInputStr = Console.ReadLine();
+void PrintAllWeapons()
 {
-    Console.WriteLine(weapon);
+    foreach (Weapon weapon in weapons)
+    {
+        Console.WriteLine(weapon);
+    }
 }
+
