0da42d68a827fe7c2d289f885aaf1de3661018d5
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-21 23:07:44 -0300

    ler input, escolher algoritmo, printar listas

--- WeaponSorting/Program.cs
+++ WeaponSorting/Program.cs
@@ -1,4 +1,26 @@
-﻿using WeaponSorting;
+﻿/* LOGICA DO SORTING
+
+// SortData recebe a lista de armas
+
+ -loop 1-
+ cria uma copia da original
+ ordena pela ordem alfabetica
+ armazena ela em sortedLists<List>
+-loop 2-
+ cria uma copia da original
+ ordena por raridade
+ armazena ela em sortedLists<List>
+-loop 3-
+ cria uma copia da original
+ ordena por dano
+ armazena ela em sortedLists<List>
+
+ retorna sortedLists<List>
+
+ program:
+ printa todos os elementos da primeira, dps todos da segunda, etc */
+
+using WeaponSorting;
 
 string weaponsPath = "data/weapons.txt";
 
@@ -8,11 +30,23 @@ weapons.AddRange(FileReader.CreateWeaponList(weaponsPath));
 
 //PrintAllWeapons();
 
-// criar um while que repete essas frases até o user colocar um input valido
 Console.WriteLine("Digite qual algorítimo deve ser usado para ordenar as armas");
 Console.WriteLine("1 - Merge Sort");
-Console.WriteLine("2 - Quick Sort");
-string userInputStr = Console.ReadLine();
+Console.WriteLine("2 - Quick Sort\n");
+string? userInputStr = Console.ReadLine();
+
+if (userInputStr == "1")
+{
+    Console.WriteLine("\nMergeSort selecionado");
+    PrintSortedWeapons(SortData.MergeSort(weapons));
+}
+
+else if (userInputStr == "2")
+{
+    Console.WriteLine("\nQuickSort selecionado"); 
+    PrintSortedWeapons(SortData.QuickSort(weapons));
+}
+
 void PrintAllWeapons()
 {
     foreach (Weapon weapon in weapons)
@@ -21,3 +55,23 @@ void PrintAllWeapons()
     }
 }
 
+void PrintSortedWeapons(List<List<Weapon>> weaponList)
+{
+    for (int i = 0; i < weaponList.Count; i++)
+    {
+        switch (i)
+        {
+            case 0: Console.WriteLine("\n------------------ ORDEM ALFABÉTICA ------------------\n"); break;
+            case 1: Console.WriteLine("\n---------------------- RARIDADE ----------------------\n"); break;
+            case 2: Console.WriteLine("\n------------------------ DANO ------------------------\n"); break;
+                
+            default : Console.WriteLine("Index inválidó"); break;
+        }
+
+        for (int j = 0; j < weaponList[i].Count; j++)
+        {
+            Console.WriteLine("Teste");
+            //Console.WriteLine(weaponList[i][j]);
+        }
+    } 
+}
---
+++ WeaponSorting/SortData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponSorting
+{
+    internal class SortData
+    {
+        public static List<List<Weapon>> MergeSort(List<Weapon> weaponList)
+        {
+            List<List<Weapon>> sortedLists = new List<List<Weapon>>();
+
+            sortedLists.Add(weaponList);
+            sortedLists.Add(weaponList);
+            sortedLists.Add(weaponList);
+
+            return sortedLists;
+        }
+
+        public static List<List<Weapon>> QuickSort(List<Weapon> weaponList)
+        {
+            List<List<Weapon>> sortedLists = new List<List<Weapon>>();
+
+            return sortedLists;
+        }
+    }
+}
